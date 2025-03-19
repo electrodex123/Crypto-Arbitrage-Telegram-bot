bfb1f436ac192bca620f68991a6f4883dcd5f4b4
electrodex123 <subhajitdeb3@gmail.com>
2025-03-19 16:07:54 +0530

    Update arbitrage_replies.py

--- keyboards/reply/arbitrage_replies.py
+++ keyboards/reply/arbitrage_replies.py
@@ -1,4 +1,4 @@
-from telebot import types # noqa
+from telebot import types  # noqa
 
 
 def create_start_reply() -> types.ReplyKeyboardMarkup:
@@ -6,5 +6,8 @@ def create_start_reply() -> types.ReplyKeyboardMarkup:
     A function to initialize a reply keyboard markup with two buttons for starting and exiting.
     """
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
-    markup.row(types.KeyboardButton('Начать \U00002705'), types.KeyboardButton('Выход \U0000274E'))
+    markup.row(
+        types.KeyboardButton('Start \U00002705'),  # Начать = Start
+        types.KeyboardButton('Exit \U0000274E')   # Выход = Exit
+    )
     return markup
