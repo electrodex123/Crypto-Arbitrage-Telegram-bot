9506e75013722d8851dd5e8c0d35c5f1b5f3b694
electrodex123 <subhajitdeb3@gmail.com>
2025-03-19 16:06:13 +0530

    Update start.py

--- handlers/default_handlers/start.py
+++ handlers/default_handlers/start.py
@@ -12,20 +12,23 @@ def bot_start(message: Message):
     """
     Handles the start command for the bot. Creates a new user, or sends a welcome
     back message if the user already exists.
+
     Parameters:
     - message: Message - the message object triggering the command
     """
-    invoke_text = ('Это сервис по арбитражу криптовалют!'
-                   '/help - просмотр списка команд и их описания')
+    invoke_text = ('This is a cryptocurrency arbitrage service!\n'
+                   '/help - view the list of commands and their descriptions')
     if bd_controller.create(message):
         bot.send_message(message.chat.id, invoke_text)
     else:
         current_user = bd_controller.get(message)
-        bot.send_message(message.chat.id,
-                         f'Рад вас снова видеть, {current_user.first_name}!\n'
-                         f'Ваш последний запрос был {datetime.strftime(current_user.last_request, DATE_FORMAT_IN)}\n'
-                         f'С этого момента многое изменилось!\n\n'
-                         f'Забыли команды: /help для просмотра списка команд и их описания',
-                         reply_markup=types.ReplyKeyboardRemove())
+        bot.send_message(
+            message.chat.id,
+            f'Glad to see you again, {current_user.first_name}!\n'
+            f'Your last request was on {datetime.strftime(current_user.last_request, DATE_FORMAT_IN)}\n'
+            f'A lot has changed since then!\n\n'
+            f'Forgot the commands? Use /help to view the list of commands and their descriptions.',
+            reply_markup=types.ReplyKeyboardRemove()
+        )
         bd_controller.update_last_request_time(message)
     Logger(message).log_activity('start')
